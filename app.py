06067c9f3c10df422a43e521cc8dca729e79425b
Jaume Barbereb¡na <ubiest@hey.com>
2021-05-14 04:17:55 +0200

    fixes

--- app.py
+++ app.py
@@ -45,11 +45,12 @@ def predict(image):
     classifier_model = "model_T19-F44-LR0.003.pkl"
     IMAGE_SHAPE = (224, 224,3)
     model_inference = load_learner(model_path/classifier_model)
-    '''
+
     test_image = image.resize((224,224))
     test_image = preprocessing.image.img_to_array(test_image)
     test_image = test_image / 255.0
     test_image = np.expand_dims(test_image, axis=0)
+    '''
     class_names = [
           'Backpack',
           'Briefcase',
@@ -57,7 +58,7 @@ def predict(image):
           'Handbag',
           'Purse']
     '''
-    predictions = model_inference.predict(image)
+    predictions = model_inference.predict(test_image)
     scores = tf.nn.softmax(predictions[0])
     scores = scores.numpy()
     results = {
@@ -68,7 +69,7 @@ def predict(image):
           'Purse': 0
           }
 
-
+    print(predictions)
     result = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence."
     return predictions
 
